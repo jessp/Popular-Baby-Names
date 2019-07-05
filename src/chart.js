988361ebce827803b10eb0cd5162cf3637fb5563
Jess <jessicapeter92@gmail.com>
2019-07-05 14:06:50 +0200

    basic chart works

---
+++ src/chart.js
@@ -0,0 +1,209 @@
+import * as d3 from "d3";
+
+class Chart {
+  constructor() {
+  	this.width = 500;
+  	this.height = 300;
+  	this.data = [];
+  	this.parseTime = d3.timeParse("%Y");
+  	this.formatTime = d3.timeFormat("%Y");
+  	this.numFormat = d3.format(",");
+  	this.margin = ({top: 20, right: 20, bottom: 30, left: 50});
+    this.svg = 
+    	d3.select("#theSvg")
+    		.attr("viewBox", [0, 0, this.width, this.height]);
+    this.colours = {
+    	"athlete": "red",
+    	"notable_person": "blue",
+    	"reality_star": "green",
+    	"actor": "orange",
+    	"unknown": "purple",
+    	"music_related": "lime",
+    	"fictional_character": "turquoise"
+    }
+
+    this.group = this.svg.append("g")
+    	.attr("class", "mainGroup")
+      .attr("fill", "none")
+      .attr("stroke-width", 0.5)
+      .attr("stroke-linejoin", "round")
+      .attr("stroke-linecap", "round");
+
+    this.dot = this.svg.append("g")
+    	.attr("class", "dot")
+      .attr("display", "none");
+
+  this.dot.append("circle")
+      .attr("r", 2.5);
+
+  this.dot.append("text")
+      .style("font", "10px sans-serif")
+      .attr("text-anchor", "middle")
+      .attr("y", -8);
+
+    this.x = d3.scaleUtc()
+    	.domain([this.parseTime(1950), this.parseTime(2018)])
+    	.range([this.margin.left, this.width - this.margin.right]);
+
+    this.y = d3.scaleLinear()
+    	.range([this.height - this.margin.bottom, this.margin.top])
+
+
+    this.drawChart = this.drawChart.bind(this);
+    this.hover = this.hover.bind(this);
+    this.moved = this.moved.bind(this);
+    this.entered = this.entered.bind(this);
+    this.left = this.left.bind(this);
+
+    this.xAxis = this.svg.append("g").attr("class", "xAxis")
+    	.attr("transform", "translate(0," + (this.height - this.margin.bottom) + ")")
+    	.call(d3.axisBottom(this.x).ticks(this.width / 80).tickSizeOuter(0))
+    
+    this.yAxis = this.svg.append("g").attr("class", "yAxis")
+	    .attr("transform", "translate(" + this.margin.left + ",0)");
+	    
+    
+  }
+
+  drawChart(data){
+  	this.data = data;
+
+  	let allValues =
+  		 data.map(function(d){ return Object.values(d.years)})
+  		.reduceRight(function(a, b) { return a.concat(b); }, []);
+  	allValues = d3.set(allValues).values().map(e => parseInt(e));
+
+  	this.y
+  		.domain([0, d3.max(allValues)]).nice();
+
+  	this.yAxis 
+  	.call(d3.axisLeft(this.y))
+	    .call(g => g.select(".domain").remove())
+	    .call(g => g.select(".tick:last-of-type text").clone()
+	        .attr("x", 3)
+	        .attr("text-anchor", "start")
+	        .attr("font-weight", "bold")
+	        .text(this.data.y))
+
+  	let y = this.y;
+	let line = d3.line()
+	    .defined(function(d){
+	    	return d && d[0] && d[0]["years"] && d[0]["years"][d[1]];
+	    })
+	    .x(d => this.x(this.parseTime(parseInt(d[1]))))
+	    .y(function(d){
+	    	if (d[0] && d[0]["years"][d[1]]){
+	    		return y(d[0]["years"][d[1]]);
+	    	} else {
+	    		return y(0);
+	    	}
+	    });
+
+
+    let paths = d3.select(".mainGroup").selectAll("path")
+	    .data(data, function(d){ return d["name"];});
+
+	paths.attr("d", function(d){
+	      	return line(Object.keys(d.years).map(function(e){ return [d, e]}));
+	      });
+
+	paths.enter()
+	    .append("path")
+	    	.attr("stroke", (d) => this.colours[d.type])
+	    	.style("mix-blend-mode", "multiply")
+	    	.attr("d", function(d){
+	    		return line(Object.keys(d.years).map(function(e){ return [d, e]}));
+	    	});
+
+	paths.exit().remove();
+
+	    this.svg.call(this.hover, this.group);
+
+  }
+
+  hover(svg, path){
+
+  	if ("ontouchstart" in document){
+  		svg
+    		.style("-webkit-tap-highlight-color", "transparent")
+    		.on("touchmove", this.moved)
+    		.on("touchstart", this.entered)
+    		.on("touchend", this.left)
+  	} else {
+  		svg
+	    	.on("mousemove", this.moved)
+	    	.on("mouseenter", this.entered)
+	    	.on("mouseleave", this.left);
+	}
+
+
+  }
+
+    moved() {
+    	let dates = d3.range(1950,2019);
+	    d3.event.preventDefault();
+	    const ym = this.y.invert(d3.event.layerY);
+	    const xm = this.x.invert(d3.event.layerX);
+	    const i1 = d3.bisectLeft(dates, this.formatTime(xm), 1);
+	    const i0 = i1 - 1;
+	    let theX = this.x;
+	    const i = xm - dates[i0] > dates[i1] - xm ? i1 : i0;
+	    let newI = i + 1950;
+	    let existingData = this.data.filter(function(d){
+	    	return d.years[newI];
+	    })
+	    let colours = this.colours;
+	    if (existingData.length > 0){
+	    	const s = existingData.reduce(function(a,b){
+		    	let a1 = a.years[newI] ? a.years[newI] - ym : -1000000000;
+		    	let b1 = b.years[newI] ? b.years[newI] - ym : -1000000000;
+		    	return Math.abs(a1) < Math.abs(b1) ? a : b;
+	   		});
+
+		    d3.select(".mainGroup").selectAll("path")
+		    .attr("stroke", function(d){
+		    	return d["name"] === s["name"] ? colours[d.type] : "#ddd";
+		    })
+		    .attr("stroke-width", function(d){
+		    	return d["name"] === s["name"] ? 1 : null;
+		    })
+		    .filter(d => d["name"] === s["name"]).raise();
+		    this.dot.attr("transform", "translate(" + this.x(this.parseTime(dates[i])) + "," + this.y(s.years[newI]) + ")");
+		    this.dot.select("text").text(s.name + " - " + this.numFormat(s["years"][newI]));
+	    	d3.select(".theExplainer p").html(s.description);
+	    } else {
+	    	this.group.style("mix-blend-mode", "multiply")
+		    this.group.selectAll("path")
+		    .attr("stroke", (d) => this.colours[d.type])
+		    .attr("stroke-width", null);
+		    this.dot.attr("display", "none");
+		    d3.select(".theExplainer p").html(null);
+	    }
+	    
+  }
+
+  entered() {
+    this.group.style("mix-blend-mode", null);
+    this.group.selectAll("path")
+    	.attr("stroke", "#ddd")
+    	.attr("stroke-width", null);
+    this.dot.attr("display", null);
+  }
+
+  left() {
+    this.group.style("mix-blend-mode", "multiply")
+    this.group.selectAll("path")
+    	.attr("stroke", (d) => this.colours[d.type])
+    	.attr("stroke-width", null);
+    this.dot.attr("display", "none");
+    d3.select(".theExplainer p").html(null);
+  }
+
+
+
+
+
+
+}
+
+export default Chart;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,102 @@
-function component() {
-  const element = document.createElement('div');
+import data from "./top_names_years.json";
+import chart from "./chart.js";
+import {set} from "d3-collection";
+import {select, selectAll} from "d3-selection";
 
-  // Lodash, currently included via a script, is required for this line to work
-  element.innerHTML = "HELLO";
+//initial values
+let type = "all";
+let subtype = "all";
+let filterData = data["values"];
 
-  return element;
+//the type selector never changes
+let possible_types = 
+	set(data.values.map(function(d){ return d.type})).values();
+	possible_types.unshift("all");
+
+let select_item = 
+	select("#type")
+		.selectAll("option")
+		.data(possible_types, function(d){ return d})
+		.enter()
+		.append("option")
+		.attr("value", function(d){ return d})
+		.html(function(d){ return d.split("_").join(" ").toUpperCase()});
+
+select("#type").on("change", updateSubtypes);
+
+let myChart = new chart();
+filterMyData();
+
+function filterMyData(){
+	if (type === "all"){
+		filterData = data["values"];
+	} else if (type === "actor" || type === "unknown" || type === "unknown"){
+		filterData = data["values"].filter(function(d){
+			return d["type"] === type;
+		})
+	} else {
+		filterData = data["values"].filter(function(d){
+			if (subtype === "all"){
+				return d["type"] === type;
+			} else {
+				return d["type"] === type && d["subtype"] === subtype;
+			}
+			
+		})
+	}
+	myChart.drawChart(filterData);
 }
+		
 
-document.body.appendChild(component());
+function updateSubtypes(e){
+	type = this.value;
+	if (type === "all" || type === "actor" || type === "unknown"){
+		let possible_subvalues = ["all"];
+
+		let select_sub_item = 
+			select("#subtype")
+				.selectAll("option")
+				.data(possible_subvalues, function(d){ return d});
+
+		select_sub_item.exit().remove();
+
+
+		select_sub_item
+				.enter()
+				.append("option")
+				.attr("value", function(d){ return d})
+				.html(function(d){ return d.split("_").join(" ").toUpperCase()});
+
+		select("#subtype").property("disabled", true);
+
+	} else {
+		let filtered_subvalues = 
+			data.values.filter(function(d){ return d["type"] === type});
+
+		let possible_subvalues = 
+			set(filtered_subvalues.map(function(d){ return d.subtype})).values();
+		possible_subvalues.unshift("all");
+
+		let select_sub_item = 
+			select("#subtype")
+				.selectAll("option")
+				.data(possible_subvalues, function(d){ return d});
+
+		select_sub_item.exit().remove();
+
+		select_sub_item
+				.enter()
+				.append("option")
+				.attr("value", function(d){ return d})
+				.html(function(d){ return d.split("_").join(" ").toUpperCase()});
+
+		select("#subtype").property("disabled", false);
+
+	}
+	select("#subtype").on("change", function(d){
+		subtype = this.value;
+		filterMyData();
+	});
+	filterMyData();
+
+}
